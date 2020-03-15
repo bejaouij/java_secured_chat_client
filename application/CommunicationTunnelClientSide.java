a2380c5bc6bb2c5218736d70db5fc91cdb9f7e1e
jbejaoui <jbejaoui@kaliop.com>
2020-03-15 10:41:41 +0100

    Init encrypted communication tunnel

--- application/CommunicationTunnelClientSide.java
+++ application/CommunicationTunnelClientSide.java
@@ -1,17 +1,24 @@
 package application;
 
+import helper.encryption.CryptoManager;
+import helper.encryption.CryptoManagerFactory;
+import helper.encryption.RSACryptoManager;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.Key;
+import java.util.Arrays;
 
 public class CommunicationTunnelClientSide implements CommunicationTunnel {
-    private static final String REMOTE_HOST = "127.0.0.1";
+    private static final String REMOTE_HOST = "192.168.43.20";
     private static final int REMOTE_PORT = 8082;
     private Socket remoteSocket;
     private BufferedReader receiverBuffer;
     private PrintWriter emitterBuffer;
+    private CryptoManager communicationCrypto;
 
     @Override
     public void init() {
@@ -24,14 +31,22 @@ public class CommunicationTunnelClientSide implements CommunicationTunnel {
 
             emitterBuffer = new PrintWriter(remoteSocket.getOutputStream());
             receiverBuffer = new BufferedReader(new InputStreamReader(remoteSocket.getInputStream()));
+            communicationCrypto = CryptoManagerFactory.getDESCryptoManager();
 
-            emitterBuffer.println("Test envoi");
+            String publicKeyString = receiverBuffer.readLine();
+
+            RSACryptoManager keyEncryptor = (RSACryptoManager) CryptoManagerFactory.getRSACryptoManager();
+            Key publicServerKey = keyEncryptor.parseBytesToKey(publicKeyString.getBytes());
+            byte[] encryptedCommunicationKey = keyEncryptor.encrypt(communicationCrypto.getSymmetricEncryptionKey().getEncoded(), publicServerKey);
+
+            emitterBuffer.println(Arrays.toString(encryptedCommunicationKey));
             emitterBuffer.flush();
 
+            //#TODO JUSTE POUR LE TEST A REMOVE
             String message;
 
             while ((message = receiverBuffer.readLine()) != null) {
-                System.out.println("server: " + message);
+                System.out.println("server: " + Arrays.toString(communicationCrypto.decrypt(message.getBytes())));
             }
 
             emitterBuffer.close();
--- application/CommunicationTunnelServerSide.java
+++ application/CommunicationTunnelServerSide.java
@@ -1,18 +1,25 @@
 package application;
 
+import helper.encryption.CryptoManager;
+import helper.encryption.CryptoManagerFactory;
+import helper.encryption.RSACryptoManager;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
+import java.util.Arrays;
 
 public class CommunicationTunnelServerSide implements CommunicationTunnel {
-    private static final int SERVER_PORT = 8081;
+    private static final int SERVER_PORT = 8082;
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private BufferedReader receiverBuffer;
     private PrintWriter emitterBuffer;
+    private CryptoManager communicationCrypto;
 
     @Override
     public void init() {
@@ -26,6 +33,24 @@ public class CommunicationTunnelServerSide implements CommunicationTunnel {
             clientSocket = serverSocket.accept();
             emitterBuffer = new PrintWriter(clientSocket.getOutputStream());
             receiverBuffer = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+
+            RSACryptoManager keyEncryptor = (RSACryptoManager) CryptoManagerFactory.getRSACryptoManager();
+            emitterBuffer.println(Arrays.toString(keyEncryptor.getAsymmetricPublicKey().getEncoded()));
+            emitterBuffer.flush();
+
+
+            String encryptedCommunicationCryptoKeyString = receiverBuffer.readLine();
+            byte[] communicationCryptoKeyContent = keyEncryptor.decrypt(encryptedCommunicationCryptoKeyString.getBytes());
+
+            Key communicationCryptoKey = communicationCrypto.parseBytesToKey(communicationCryptoKeyContent);
+            communicationCrypto = CryptoManagerFactory.getDESCryptoManager(communicationCryptoKey);
+
+            String messageClair = "Coucou";
+            emitterBuffer.println(Arrays.toString(communicationCrypto.encrypt(messageClair.getBytes())));
+            messageClair = "World!";
+            emitterBuffer.println(Arrays.toString(communicationCrypto.encrypt(messageClair.getBytes())));
+            messageClair = "Oui";
+            emitterBuffer.println(Arrays.toString(communicationCrypto.encrypt(messageClair.getBytes())));
 
             String message;
 
